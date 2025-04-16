a3b98839b2c542500dc6e875421d6af4a9e638f4
renatopoli <32083066+renatopoli@users.noreply.github.com>
2025-04-16 15:02:16 -0300

    continuação dos ajustes de front

--- frontend/views/avulsos/section-quem-ja-confiou.php
+++ frontend/views/avulsos/section-quem-ja-confiou.php
@@ -18,19 +18,20 @@
  */
 extract($args);
 ?>
-<section class="section-quem-ja-confiou gap-md padding-y-xl padding-y-xxxl@md">
-  <div class="max-width-lg container gap-sm gap-xl@md justify-start items-center">
+<section class="section-quem-ja-confiou padding-y-xl padding-y-xxxl@md">
+  <div class="flex flex-column max-width-lg container gap-sm gap-xl@md justify-start items-center">
 
-    <h1 class="section-quem-ja-confiou__titulo-secao">Clientes</h1>
-    <h2 class="section-quem-ja-confiou__subtitulo-secao padding-xl"> Quem já confiou na nossa experiência</h2>
-
-      <div class="grid gap-lg items-center">
-        <?php foreach ($itens as $item): if (empty($item)) continue; ?>
-          <div class="col-6 col-3@md flex@md">
-            <?= $item; ?>
-          </div>
-        <?php endforeach; ?>
-      </div>
+    <div class="flex flex-column gap-xs">
+      <h1 class="section-quem-ja-confiou__titulo-secao">Clientes</h1>
+      <h2 class="section-quem-ja-confiou__subtitulo-secao"> Quem já confiou na nossa experiência</h2>
+    </div>
+    <div class="grid gap-lg items-center">
+      <?php foreach ($itens as $item): if (empty($item)) continue; ?>
+        <div class="col-6 col-3@md flex@md">
+          <?= $item; ?>
+        </div>
+      <?php endforeach; ?>
+    </div>
 
   </div>
 </section>
