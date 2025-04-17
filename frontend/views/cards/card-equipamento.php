b9f6d5946f100cc2cc2a7c42cdcfaf7fcfe1f53e
renatopoli <32083066+renatopoli@users.noreply.github.com>
2025-04-17 16:44:06 -0300

    ajustes front

--- frontend/views/cards/card-equipamento.php
+++ frontend/views/cards/card-equipamento.php
@@ -13,12 +13,12 @@
  */
 extract($args);
 ?>
-<div class="card-equipamento col-12 col-4@md swiper-slide">
+<div class="card-equipamento col-12 col-4@md swiper-slide bg-cinza-claro">
     <figure class="equipamento__imagem">
-        <img src="<?= $imagem; ?>">
+        <img src="<?= $imagem; ?>" class="radius-lg radius-bottom-right-0 radius-bottom-left-0">
     </figure>
 
-    <div class="padding-md">
+    <div class="padding-md flex flex-column gap-xxs">
         <h3 class="card-equipamento__titulo"><?= $titulo; ?></h3>
         <p class="card-equipamento__texto"><?= $texto; ?></h3>
     </div>
